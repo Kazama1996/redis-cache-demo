59003a2afae712da0f25392a06745455aa0170ec
Kazama1996 <y.kazama1996@gmail.com>
2026-03-25 23:20:17 +0800

    create order_created_outbox table sql

---
+++ src/main/resources/db/create_order_created_outbox.sql
@@ -0,0 +1,8 @@
+CREATE TABLE order_created_outbox(
+                       id BIGINT PRIMARY KEY ,
+                       topic_name VARCHAR(255) NOT NULL,
+                       payload JSONB NOT NULL,
+                       status VARCHAR(100) NOT NULL,
+                       created_at          TIMESTAMPTZ     NOT NULL,
+                       updated_at          TIMESTAMPTZ     NOT NULL
+)
